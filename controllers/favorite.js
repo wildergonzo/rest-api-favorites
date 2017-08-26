b73d6cba7a042a9fa545d66f7142d93a9c738c8e
Wilder Gonzales <wilder.gonzales@gmail.com>
2017-08-26 14:44:22 +0200

    basic api crud

--- controllers/favorite.js
+++ controllers/favorite.js
@@ -1,8 +1,34 @@
-function user(req, res){
+function test(req, res){
 	var name;
     req.params.name? name=req.params.name : name='guest';
     res.status(200).
         send('hello ' + name);
 }
 
-module.exports = {user}
+function getUser(req, res){
+	var id = req.params.id;
+	res.status(200).send({"id": id, "key": "value"});
+}
+
+function setUser(req, res){
+    var params = req.body;
+    res.status(200).send({favorites: params});
+}
+
+function updateUser(req, res){
+    var params = req.body;
+    res.status(200).send({update: true, favorites: params});
+}
+
+function deleteUser(req, res){
+    var id = req.params.id;
+    res.status(200).send({delete: true, data: id});
+}
+
+module.exports = {
+	test,
+	getUser,
+	setUser,
+	updateUser,
+	deleteUser
+}
--- routes/favorite.js
+++ routes/favorite.js
@@ -2,6 +2,10 @@ var express = require('express');
 var FavoriteController = require('../controllers/favorite');
 var api = express.Router();
 
-api.get('/user/:name?', FavoriteController.user);
+api.get('/test/:name?', FavoriteController.test);
+api.get('/user/:id', FavoriteController.getUser);
+api.post('/user', FavoriteController.setUser);
+api.put('/user', FavoriteController.updateUser);
+api.delete('/user/:id', FavoriteController.deleteUser);
 
 module.exports = api;
